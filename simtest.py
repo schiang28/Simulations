8edce538a973c6ab6b4117699b46f1b768769f18
Sophie Chiang <sophieyoshi2014@gmail.com>
2022-06-10 21:41:47 +0100

    populationi simulation test

--- simtest.py
+++ simtest.py
@@ -1,4 +1,5 @@
 import random
+
 startPopulation = 50
 infantMortality = 5
 youthMortality = 45
@@ -8,47 +9,65 @@ fertilityx = 18
 fertilityy = 35
 food = 0
 peopleDictionary = []
+
+
 class Person:
     def __init__(self, age):
-        self.gender = random.randint(0,1)
+        self.gender = random.randint(0, 1)
         self.age = age
         self.pregnant = 0
+
+
 def harvest(food, agriculture):
     ablePeople = 0
     for person in peopleDictionary:
         if person.age > 8:
-            ablePeople +=1
-food += ablePeople * agriculture
+            ablePeople += 1
+    food += ablePeople * agriculture
+
+
 if food < len(peopleDictionary):
-        del peopleDictionary[0:int(len(peopleDictionary)-food)]
-        food = 0
-    else:
-        food -= len(peopleDictionary)
+    del peopleDictionary[0 : int(len(peopleDictionary) - food)]
+    food = 0
+else:
+    food -= len(peopleDictionary)
+
+
 def reproduce(fertilityx, fertilityy, infantMortality):
     for person in peopleDictionary:
         if person.gender == 1:
             if person.age > fertilityx:
                 if person.age < fertilityy:
-                    if random.randint(0,5)==1:
-                        if random.randint(0,100)>infantMortality:
+                    if random.randint(0, 5) == 1:
+                        if random.randint(0, 100) > infantMortality:
                             peopleDictionary.append(Person(0))
+
+
 def beginSim():
     for x in range(startPopulation):
-        peopleDictionary.append(Person(random.randint(18,50)))
+        peopleDictionary.append(Person(random.randint(18, 50)))
+
+
 def runYear(food, agriculture, fertilityx, fertilityy, infantMortality, disasterChance):
     harvest(food, agriculture)
     for person in peopleDictionary:
         if person.age > 80:
             peopleDictionary.remove(person)
         else:
-            person.age +=1
-    
+            person.age += 1
+
     reproduce(fertilityx, fertilityy, infantMortality)
-if random.randint(0,100)<disasterChance:
-        del peopleDictionary[0:int(random.uniform(0.05,0.2)*len(peopleDictionary))]
+
+    if random.randint(0, 100) < disasterChance:
+        del peopleDictionary[0 : int(random.uniform(0.05, 0.2) * len(peopleDictionary))]
     print(len(peopleDictionary))
     infantMortality *= 0.985
     return infantMortality
+
+
 beginSim()
-while len(peopleDictionary)<100000 and len(peopleDictionary) > 1:
-    infantMortality = runYear(food, agriculture, fertilityx, fertilityy, infantMortality, disasterChance)
+while len(peopleDictionary) < 100000 and len(peopleDictionary) > 1:
+    infantMortality = runYear(
+        food, agriculture, fertilityx, fertilityy, infantMortality, disasterChance
+    )
+
