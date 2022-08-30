9ee4ac2f42509eb65ff4e6a9a58a8e822493fc49
Sophie Chiang <sophieyoshi2014@gmail.com>
2022-08-30 14:13:30 +0100

    platonic solids

---
+++ platonicsolids.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+import pyvista as pv
+from pyvista import examples
+
+kinds = [
+    "tetrahedron",
+    "cube",
+    "octahedron",
+    "dodecahedron",
+    "icosahedron",
+]
+centers = [
+    (0, -2, 0),
+    (0, -1, 0),
+    (0, 0, 0),
+    (0, 1, 0),
+    (0, 2, 0),
+]
+
+colors = ["red", "yellow", "green", "blue", "purple"]
+
+solids = [
+    pv.PlatonicSolid(kind, radius=0.4, center=center)
+    for kind, center in zip(kinds, centers)
+]
+
+p = pv.Plotter(window_size=[1000, 1000])
+for ind, solid in enumerate(solids):
+    smooth_shading = ind == len(solids) - 1
+    p.add_mesh(
+        solid,
+        color=colors[ind],
+        smooth_shading=smooth_shading,
+        specular=1.0,
+        specular_power=10,
+        show_edges=True,
+    )
+p.view_vector((5.0, 2, 3))
+p.add_floor("-z", lighting=True, color="tan", pad=1.0)
+p.enable_shadows()
+p.show()
